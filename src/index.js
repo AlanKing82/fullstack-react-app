0428bc048e9019bf9d6dc484da46e4718d505105
Alan King(2) <Alan King(2)>
2017-03-27 12:02:49 +0100

    react set-up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-ECHO is on.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    React.createElement('h2', null, 'Hello React'),
+    document.getElementById('root')
+)
